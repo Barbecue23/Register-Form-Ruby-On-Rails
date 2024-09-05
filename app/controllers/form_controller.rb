1481d47795774bbd9ce69b11b695576b7e5634f9
Barbecue <sunsanasupphong_j@silpakorn.edu>
2024-09-05 08:35:55 +0700

    Create form page and set path

--- app/controllers/form_controller.rb
+++ app/controllers/form_controller.rb
@@ -1,2 +1,17 @@
 class FormController < ApplicationController
+  def new
+    @form = Form.new
+  end
+  def create
+    @form = Form.new(form_params)
+    if @form.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+  private
+  def form_params
+    params.require(:form).permit(:firstname, :lastname, :birthdate, :gender, :email, :phonenumber, :subject)
+  end
 end
