c9c1b77f97973bb71d20e864825631d7d58be333
Barbecue <sunsanasupphong_j@silpakorn.edu>
2024-09-05 22:24:00 +0700

    add delete data button and edit styles index page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get "form/new" ,to: "form#new"
   get "form/index" ,to: "form#index"
   post "form" ,to: "form#create"
-  delete "form" ,to: "form#destroy[:id]"
+  delete "form/:id" ,to: "form#destroy"
   root "form#index"
   # resources :form , only: [:new, :create]
 end
